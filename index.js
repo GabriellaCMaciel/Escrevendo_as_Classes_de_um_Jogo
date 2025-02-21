77d2d295e299e8be2e01f163a3657e25af655a12
GabriellaCMaciel <gabriellaacunhamaciel@gmail.com>
2025-02-21 13:53:23 -0300

    att final

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-class hero{
-    constructor(name, age, type){
+class hero {
+    constructor(name, age, type) {
         this.name = name
         this.age = age
         this.type = type
     }
-    
-    atack(){
-        console.log(`o ${type} atacou usando ${atack}`)
+
+    attack() {
+        if (this.type === "mago") {
+            return "magia";
+        }
+        else if (this.type === "guerreiro") {
+            return "espada";
+        }
+        else if (this.type === "monge") {
+            return "artes marciais";
+        }
+        else if (this.type === "ninja") {
+            return "shuriken";
+        }
     }
 }
 
 let hero1 = new hero("Zhongli", 1000, "guerreiro")
 let hero2 = new hero("Lisa", 27, "mago")
+let hero3 = new hero("Endaiel", 10903, "monge")
+let hero4 = new hero("Naruto", 36, "ninja")
 
-hero1
+console.log(`O ${hero1.type} atacou usando ${hero1.attack()}`);
+console.log(`O ${hero2.type} atacou usando ${hero2.attack()}`);
+console.log(`O ${hero3.type} atacou usando ${hero3.attack()}`);
+console.log(`O ${hero4.type} atacou usando ${hero4.attack()}`);
