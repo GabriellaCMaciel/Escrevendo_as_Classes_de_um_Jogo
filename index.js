d0b9d419823902e7782d779133a8789d813a195e
GabriellaCMaciel <gabriellaacunhamaciel@gmail.com>
2025-02-21 12:39:45 -0300

    att

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 class hero{
-    constructor hero
-    this.nome = 
+    constructor(name, age, type){
+        this.name = name
+        this.age = age
+        this.type = type
+    }
+    
+    atack(){
+        console.log(`o ${type} atacou usando ${atack}`)
+    }
 }
+
+let hero1 = new hero("Zhongli", 9.000, "guerreiro")
+let hero2 = new hero("Lisa", 27, "mago")
+
+hero1
